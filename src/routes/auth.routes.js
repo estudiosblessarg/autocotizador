57634d91ffe3b142fdd5627647cf158c6b60d2e3
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 20:42:29 -0300

    Add files via upload

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,12 +2,53 @@ const express = require("express")
 const router = express.Router()
 
 const authController = require("../controllers/auth.controller")
+const cotizadorController = require("../controllers/cotizar.controller")
+const cotizacionController = require("../controllers/cotizacion.controller")
+const userController = require("../controllers/user.controller")
+const vehiculoController = require("../controllers/vehiculo.controller")
+const configController = require("../controllers/config.controller")
 
-// =================
+// ======================
 // AUTH
-// =================
+// ======================
 
 router.post("/login",authController.login)
 router.post("/register",authController.register)
+router.get("/users",userController.getUsers)
+router.put("/users/:id",userController.updateRole)
+
+
+// ======================
+// COTIZADOR
+// ======================
+
+router.post("/cotizar",cotizadorController.cotizar)
+
+
+// ======================
+// COTIZACIONES ADMIN
+// ======================
+
+router.get("/cotizaciones",cotizacionController.getAll)
+router.put("/cotizaciones/:id",cotizacionController.update)
+router.delete("/cotizaciones/:id",cotizacionController.delete)
+
+
+// ======================
+// CONFIG
+// ======================
+
+router.get("/km",configController.getKM)
+router.put("/km",configController.updateKM)
+
+
+// ======================
+// VEHICULOS
+// ======================
+
+router.get("/cotizador/marcas",vehiculoController.getMarcas)
+router.get("/cotizador/modelos/:marca",vehiculoController.getModelos)
+router.get("/cotizador/versiones/:marca/:modelo",vehiculoController.getVersiones)
+router.get("/cotizador/anios/:marca/:modelo/:version",vehiculoController.getAnios)
 
 module.exports = router
