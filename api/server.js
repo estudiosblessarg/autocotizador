c3ab1b63f4f9a7059e2188a3ac1adc7c9c07870a
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 20:00:33 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,47 @@
 const express = require("express")
 const serverless = require("serverless-http")
 
-const authRoutes = require("../src/routes/auth.routes")
 const { db } = require("../src/config/firebase")
 
 const app = express()
 
 app.use(express.json())
 
-app.use("/auth", authRoutes)
+// test firebase
+app.get("/status", async (req,res)=>{
+  await db.collection("test").limit(1).get()
+  res.status(200).end()
+})
+
+// REGISTER
+app.post("/auth/register", async (req,res)=>{
+
+  const { email, password, role } = req.body
+
+  try{
+
+    const user = {
+      email,
+      password,
+      role,
+      createdAt: new Date()
+    }
+
+    const ref = await db.collection("users").add(user)
+
+    res.json({
+      success:true,
+      id:ref.id
+    })
+
+  }catch(err){
+
+    res.status(500).json({
+      error:"error creando usuario"
+    })
+
+  }
+
+})
 
 module.exports = serverless(app)
