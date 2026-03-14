a9866e1ac612b2c38e644e449be228a635f6bb20
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 19:36:26 -0300

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const serverless = require("serverless-http")
-const authRoutes = require("../src/routes/auth.routes")
 
+const authRoutes = require("../src/routes/auth.routes")
 const { db } = require("../src/config/firebase")
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/api/status", async (req,res)=>{
+app.get("/status", async (req,res)=>{
 
 await db.collection("test").limit(1).get()
 
@@ -17,7 +17,5 @@ res.status(200).end()
 })
 
 app.use("/auth", authRoutes)
-
-
 
 module.exports = serverless(app)
