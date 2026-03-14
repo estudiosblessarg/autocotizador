41c3e42294f65549d11b7197ad96501ea0a33eb1
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 18:59:48 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,21 +1,22 @@
 const express = require("express")
 const serverless = require("serverless-http")
 
-const authRoutes = require("../src/routes/auth.routes")
+//const authRoutes = require("../src/routes/auth.routes")
 
 const app = express()
 
 app.use(express.json())
 
 // ROUTES
-app.use("/api/auth", authRoutes)
+//app.use("/api/auth", authRoutes)
 
 app.get("/api/status",(req,res)=>{
 
 console.log("STATUS ENDPOINT")
 
 res.json({
-ok:true
+ok:true,
+res: "Api funcionando"
 })
 
 })
