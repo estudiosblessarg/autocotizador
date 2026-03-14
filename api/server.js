df89c546ce772623dfff388563fa9463aec88f80
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 18:31:41 -0300

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname,"../public")))
 
 app.get("/",(req,res)=>{
 
- res.sendFile(path.join(__dirname,"../public/index.html"))
+ res.sendFile(path.join(__dirname,"../../public/index.html"))
 
 })
 
