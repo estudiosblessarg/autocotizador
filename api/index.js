55ba5f1872100d3e4279cfc403c163bf63ee8641
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 18:51:21 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,39 +1,23 @@
-require("dotenv").config()
-
 const express = require("express")
-const cors = require("cors")
-const path = require("path")
 const serverless = require("serverless-http")
 
-const routes = require("../src/routes/auth.routes")
+const authRoutes = require("../src/routes/auth.routes")
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
-// =================
-// API
-// =================
+// ROUTES
+app.use("/api/auth", authRoutes)
 
-app.use("/api", routes)
+app.get("/api/status",(req,res)=>{
 
-app.get("/api/status", (req,res)=>{
-  res.json({
-    ok:true,
-    message:"API funcionando"
-  })
+console.log("STATUS ENDPOINT")
+
+res.json({
+ok:true
 })
 
-
-// =================
-// FRONTEND
-// =================
-
-app.use(express.static(path.join(__dirname,"../public")))
-
-app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname,"../public/index.html"))
 })
 
 module.exports = serverless(app)
