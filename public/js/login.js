0eb748c52e25b4198b5a54ecbf528160f5b569fc
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 20:14:40 -0300

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ try{
 
 console.log("🚀 Enviando request a /api/auth/login")
 
-const res = await fetch("/api/auth/login",{
+const res = await fetch("/auth/login",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
