f9028536015def193184b6976730b0cc6040db60
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 19:48:57 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,20 +8,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/status", async (req,res)=>{
-
-await db.collection("test").limit(1).get()
-
-res.status(200).json("Database cargada correctamente").end()
-
-})
-app.post("/auth/register",(req,res)=>{
-    if (!res.ok) {
-        res.json("Ruta no encontrada")
-    }
-  res.json({ok:true})
-})
-
-//app.use("/auth", authRoutes)
+app.use("/auth", authRoutes)
 
 module.exports = serverless(app)
