eccecc2010ed9bc12a29f0eff963e3329966c060
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-14 22:54:10 -0300

    Update vehiculo.controller.js

--- src/controllers/vehiculo.controller.js
+++ src/controllers/vehiculo.controller.js
@@ -1,50 +1,186 @@
-const fs = require("fs")
-const path = require("path")
+const axios = require("axios")
+const { db } = require("../config/firebase")
 
-const dataPath = path.join(__dirname,"../../public/data/vehiculos.json")
+/*
+========================================
+BUSCAR EN INTERNET SI NO EXISTE
+========================================
+*/
 
-function load(){
+async function buscarEnInternet(marca,modelo){
 
- return JSON.parse(fs.readFileSync(dataPath))
+ try{
 
-}
+  const url = "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/"+marca+"?format=json"
 
+  const res = await axios.get(url)
 
-exports.getMarcas = (req,res)=>{
+  const modelos = res.data.Results.map(m=>m.Model_Name)
 
- const data = load()
+  return modelos
 
- res.json(Object.keys(data))
+ }catch(err){
 
-}
+  console.error("Error buscando en internet",err)
+  return []
 
-
-exports.getModelos = (req,res)=>{
-
- const data = load()
-
- res.json(Object.keys(data[req.params.marca] || {}))
-
-}
-
-
-exports.getVersiones = (req,res)=>{
-
- const data = load()
-
- const {marca,modelo} = req.params
-
- res.json(Object.keys(data[marca][modelo] || {}))
+ }
 
 }
 
 
-exports.getAnios = (req,res)=>{
+/*
+========================================
+GET MARCAS
+========================================
+*/
 
- const data = load()
+exports.getMarcas = async(req,res)=>{
 
- const {marca,modelo,version} = req.params
+ try{
 
- res.json(data[marca][modelo][version] || [])
+  const snap = await db.collection("vehiculos").get()
+
+  const marcas = snap.docs.map(d=>d.id)
+
+  res.json(marcas)
+
+ }catch(err){
+
+  console.error(err)
+  res.status(500).json({error:"error obteniendo marcas"})
+
+ }
+
+}
+
+
+/*
+========================================
+GET MODELOS
+========================================
+*/
+
+exports.getModelos = async(req,res)=>{
+
+ try{
+
+  const {marca} = req.params
+
+  const snap = await db
+   .collection("vehiculos")
+   .doc(marca)
+   .collection("modelos")
+   .get()
+
+  if(!snap.empty){
+
+   const modelos = snap.docs.map(d=>d.id)
+   return res.json(modelos)
+
+  }
+
+  // si no existe buscar en internet
+
+  console.log("Buscando modelos en internet")
+
+  const modelosInternet = await buscarEnInternet(marca)
+
+  // guardar en firestore
+
+  for(const modelo of modelosInternet){
+
+   await db
+    .collection("vehiculos")
+    .doc(marca)
+    .collection("modelos")
+    .doc(modelo)
+    .set({created:true})
+
+  }
+
+  res.json(modelosInternet)
+
+ }catch(err){
+
+  console.error(err)
+  res.status(500).json({error:"error obteniendo modelos"})
+
+ }
+
+}
+
+
+/*
+========================================
+GET VERSIONES
+========================================
+*/
+
+exports.getVersiones = async(req,res)=>{
+
+ try{
+
+  const {marca,modelo} = req.params
+
+  const snap = await db
+   .collection("vehiculos")
+   .doc(marca)
+   .collection("modelos")
+   .doc(modelo)
+   .collection("versiones")
+   .get()
+
+  const versiones = snap.docs.map(d=>d.id)
+
+  res.json(versiones)
+
+ }catch(err){
+
+  console.error(err)
+  res.status(500).json({error:"error versiones"})
+
+ }
+
+}
+
+
+/*
+========================================
+GET AÑOS
+========================================
+*/
+
+exports.getAnios = async(req,res)=>{
+
+ try{
+
+  const {marca,modelo,version} = req.params
+
+  const doc = await db
+   .collection("vehiculos")
+   .doc(marca)
+   .collection("modelos")
+   .doc(modelo)
+   .collection("versiones")
+   .doc(version)
+   .get()
+
+  if(!doc.exists){
+
+   return res.json([])
+
+  }
+
+  const data = doc.data()
+
+  res.json(data.anios || [])
+
+ }catch(err){
+
+  console.error(err)
+  res.status(500).json({error:"error años"})
+
+ }
 
 }
