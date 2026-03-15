b6d82861d811ec4a14a8709e197f2dcc5054783a
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-15 17:30:48 -0300

    Add files via upload

---
+++ src/controllers/cotizacion.controller.js
@@ -0,0 +1,68 @@
+const {db} = require("../config/firebase")
+
+exports.getAll = async(req,res)=>{
+
+ try{
+
+  const snap = await db.collection("cotizaciones").get()
+
+  const data = snap.docs.map(d=>({
+   id:d.id,
+   ...d.data()
+  }))
+
+  res.json(data)
+
+ }catch(error){
+
+  res.status(500).json({
+   error:error.message
+  })
+
+ }
+
+}
+
+
+
+exports.update = async(req,res)=>{
+
+ try{
+
+  const {id} = req.params
+
+  await db.collection("cotizaciones").doc(id).update(req.body)
+
+  res.json({success:true})
+
+ }catch(error){
+
+  res.status(500).json({
+   error:error.message
+  })
+
+ }
+
+}
+
+
+
+exports.delete = async(req,res)=>{
+
+ try{
+
+  const {id} = req.params
+
+  await db.collection("cotizaciones").doc(id).delete()
+
+  res.json({success:true})
+
+ }catch(error){
+
+  res.status(500).json({
+   error:error.message
+  })
+
+ }
+
+}
