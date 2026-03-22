2936f53477479b4f1fa9f1c49275628ce86cbd3b
estudiosblessarg <estudiosblessarg@gmail.com>
2026-03-22 18:59:13 -0300

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -335,7 +335,7 @@ async function agregarKM(){
  const km = document.getElementById("kmValor").value
  const descuento = document.getElementById("kmDesc").value
 
- const config = await api("/km")
+ const config = await api("auth/km")
 
  config.tabla.push({
   km:Number(km),
